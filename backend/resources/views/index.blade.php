c8f4bae56c2f02363908042aad60b39f2118b459
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-08 08:56:05 +0900

    トップページの追加

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -1,2 +1,22 @@
 @extends('layouts.app')
 
+@section('content')
+<div class="row justify-content-center">
+  <div class="col-md-4">
+    <div class="card mb50">
+      <div class="card-body">
+        <div class='image-wrapper'>
+          <img class='book-image' src="{{ asset('images/dummy.png') }}" width="150px" height="auto">
+        </div>
+        <h3 class='h3 book-title'>タイトル</h3>
+        <p class='description'>
+          レビュー本文
+        </p>
+        <a href=""class='btn btn-secondary detail-btn'>
+          詳細を読む
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
