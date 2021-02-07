478f5fe80e4bdf51b942cc603a357ca895dcf559
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-07 19:46:13 +0900

    ルーティングの設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [ReviewController::class, 'index'])->name('index');
