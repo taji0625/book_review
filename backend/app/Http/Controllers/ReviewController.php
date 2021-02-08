34dbe45d77f3ed457815c0bf4466cbfb62b5e895
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-08 15:28:48 +0900

    ReviewControllerにバリデーションを導入

--- backend/app/Http/Controllers/ReviewController.php
+++ backend/app/Http/Controllers/ReviewController.php
@@ -21,6 +21,12 @@ class ReviewController extends Controller
     {
         $post = $request->all();
 
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'image' => 'mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
         if($request->hasFile('image')) {
             $request->file('image')->store('/public/images');
             $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'], 'image' => $request->file('image')->hashName()];
